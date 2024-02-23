9170ecfa4adb8c4f2ee2c095375eef18b695668d
Vinicyus Silva <vinicyus346@gmail.com>
2024-02-23 11:22:59 -0300

    feat: listing tags page

---
+++ src/app.tsx
@@ -0,0 +1,162 @@
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
+import { FileDown, Filter, MoreHorizontal, Plus, Search } from 'lucide-react'
+import { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { Header } from './components/header'
+import { Pagination } from './components/pagination'
+import { Tabs } from './components/tabs'
+import { Button } from './components/ui/button'
+import { Control, Input } from './components/ui/input'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
+import * as Dialog from '@radix-ui/react-dialog'
+import { CreateTagForm } from './components/create-tag-form'
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  slug: string
+  amountOfVideos: number
+  id: string
+}
+
+
+export function App() {
+  const [ searchParams, setSearchParams] = useSearchParams()
+  const urlFilter = searchParams.get('filter') ?? ''
+  const [filter, setFilter] = useState(urlFilter)
+
+  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1
+  const per_page = searchParams.get('per_page') ? Number(searchParams.get('per_page')) : 10
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=${per_page}&title=${urlFilter}`)
+      const data = await response.json()      
+
+      return data
+    },
+    queryKey: ['get-tags', urlFilter, page, per_page],
+    placeholderData: keepPreviousData
+  })
+
+  if (isLoading) {
+    return null
+  }
+
+  function handleFilter () {
+    setSearchParams((params) => {
+      params.set('page', '1')
+      params.set('filter', filter)
+      return params
+    })
+  }
+
+  return(
+    <div className="py-10 space-y-8">
+      <div>
+        <Header />
+        <Tabs />        
+      </div>
+
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className='flex items-center gap-3'>
+          <h1 className="text-xl font-bold">Tags</h1>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button variant='primary'>
+                <Plus className='size-3'/>
+                Create new
+              </Button>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className='fixed inset-0 bg-black/70' />
+              <Dialog.Content className='fixed p-10 space-y-10 right-0 top-0 bottom-0 h-screen min-w-[320px] z-10 bg-zinc-950 border-l border-zinc-900'>
+                <div className='space-y-3'>
+                  <Dialog.Title className='text-xl font-bold'>
+                    Create Tag
+                  </Dialog.Title>
+                  <Dialog.Description className='text-sm text-zinc-500'>
+                    Tags can be used to group videos about similar concepts
+                  </Dialog.Description>
+                </div>
+
+                <CreateTagForm />
+                <Dialog.Close />
+              </Dialog.Content>              
+            </Dialog.Portal>
+          </Dialog.Root>          
+        </div>
+        
+        <div className='flex items-center justify-between'>
+          <div className="flex items-center">
+            <Input variant='filter'>
+              <Search className='size-3' />
+              <Control placeholder='Search tags...' 
+                onChange={(e) => setFilter(e.target.value)}
+                value={filter}
+              />
+            </Input>
+
+            <Button onClick={handleFilter} type='submit'>
+              <Filter className='size-3' />
+              Filter 
+            </Button>
+          </div>
+
+          <Button>
+            <FileDown className='size-3' />
+            Export 
+          </Button>
+        </div>
+
+        <Table> 
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead> Tag </TableHead>
+              <TableHead> Amount of videos </TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {
+              tagsResponse?.data.map((tag) => {
+                return (
+                  <TableRow key={tag.id}>
+                    <TableCell></TableCell>
+                    <TableCell>
+                      <div className='flex flex-col gap-0.5'>
+                        <span className='font-medium'>{tag.title}</span>
+                        <span className='text-xs text-zinc-500'>{tag.slug}</span>
+                      </div>
+                    </TableCell>
+                    <TableCell className='text-zinc-500'>
+                      {tag.amountOfVideos} video(s)
+                    </TableCell>
+                    <TableCell className='text-right'>
+                      <Button size='icon'>
+                        <MoreHorizontal className='size-4'/>
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                )
+              })
+            }
+          </TableBody>
+        </Table>
+          
+        {tagsResponse && <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={page} per_page={per_page}/>}        
+      </main>
+    </div>
+  )
+}
---
+++ src/components/create-tag-form.tsx
@@ -0,0 +1,95 @@
+import { Check, Loader2, X } from "lucide-react";
+import { Button } from "./ui/button";
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import * as Dialog from '@radix-ui/react-dialog'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+const createTagSchema = z.object({
+  title: z.string().min(3, { message: 'Minimum 3 characters'})  
+})
+
+type CreateTagSchema = z.infer<typeof createTagSchema>
+
+function getSlugFromString(input: string): string {  
+  return input
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .replace(/[^\w\s]/g, "")
+    .replace(/\s+/g, "-")
+}
+
+export function CreateTagForm() {
+  const queryClient = useQueryClient()
+
+  const { register, handleSubmit, watch, formState } = useForm<CreateTagSchema>({
+    resolver: zodResolver(createTagSchema)
+  })
+
+  const slug = watch('title') ? getSlugFromString(watch('title')) : ''
+
+  const {mutateAsync} = useMutation({
+    mutationFn: async ({title }: CreateTagSchema) => {
+      await fetch('http://localhost:3333/tags', {
+        method: 'POST',
+        body: JSON.stringify({
+          title, 
+          slug,
+          amountOfVideos: 0
+        })
+      })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['get-tags']
+      })
+    }
+  })
+
+  async function createTag({title }: CreateTagSchema){
+    await mutateAsync({ title })
+  }  
+
+  return (
+    <form className="w-full space-y-6" onSubmit={handleSubmit(createTag)}>
+      <div className="space-y-2">
+        <label className="text-sm font-medium block" htmlFor="title"> Tag title </label>
+        <input 
+          {...register("title")}
+          id="title" 
+          type="text" 
+          className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
+        />
+        {formState.errors?.title && (
+          <p className="text-sm text-red-400">{formState.errors.title.message}</p>
+        )}
+      </div>
+
+      <div className="space-y-2">
+        <label className="text-sm font-medium block" htmlFor="slug"> Slug </label>
+        <input           
+          value={slug}
+          id="slug" 
+          type="text" 
+          className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
+          readOnly
+        />
+      </div>
+
+      <div className="flex items-center justify-end gap-2">
+        <Dialog.Close>
+          <Button>
+            <X className="size-3" />
+            Cancel
+          </Button>
+        </Dialog.Close>
+        
+        <Button disabled={formState.isSubmitting} type="submit" className="bg-teal-400 text-teal-950">
+          { formState.isSubmitting ? <Loader2 className="size-3 animate-spin" /> : <Check className="size-3" /> }
+          Save
+        </Button>
+      </div>
+    </form>
+  )
+}
